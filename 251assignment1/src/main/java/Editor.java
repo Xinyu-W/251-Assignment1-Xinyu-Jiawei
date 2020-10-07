01946cd64dc8d66859486e04467ea2059200b84c
Xinyu-W <850696481@qq.com>
2020-10-07 14:18:27 +0800

    Simplified a part of code.

--- 251assignment1/src/main/java/Editor.java
+++ 251assignment1/src/main/java/Editor.java
@@ -20,43 +20,33 @@ public class Editor extends JFrame {
         DateFormat dateFormat=DateFormat.getDateTimeInstance();
         String dateTime=dateFormat.format(System.currentTimeMillis());
         this.setTitle("Text editor           "+dateTime);
-        Action[] actions={
-                new New(textPane,fileChooser,Editor.this),
-                new Open(textPane,fileChooser,Editor.this),
-                new Save(textPane,fileChooser),
-                new Search(textPane,SearchText),
-                new Exit(Editor.this),
-                new Copy(textPane,Editor.this),
-                new Cut(textPane,Editor.this),
-                new Paste(textPane,Editor.this)
-        };
 
-        setJMenuBar(createJMenuBar(actions));
-
+        setJMenuBar(createJMenuBar());
         Container container=getContentPane();
         container.add(textPane,BorderLayout.CENTER);
         setSize(700,700);
         setVisible(true);
         }
-        private JMenuBar createJMenuBar(Action[] actions){
+
+        private JMenuBar createJMenuBar(){
             JMenuBar menuBar=new JMenuBar();
             JMenu menuFile=new JMenu("File");
             JMenu menuView=new JMenu("View");
             JMenu menuEdior=new JMenu("Editor");
             JMenu menuAbout = new JMenu("About");
-            menuFile.add(new JMenuItem(actions[0]));
-            menuFile.add(new JMenuItem(actions[1]));
-            menuFile.add(new JMenuItem(actions[2]));
-            menuView.add(new JMenuItem(actions[3]));
-            menuFile.add(new JMenuItem(actions[4]));
-            menuEdior.add(new JMenuItem(actions[5]));
-            menuEdior.add(new JMenuItem(actions[6]));
-            menuEdior.add(new JMenuItem(actions[7]));
+            menuFile.add(new JMenuItem(new New(textPane,fileChooser,Editor.this)));
+            menuFile.add(new JMenuItem(new Open(textPane,fileChooser,Editor.this)));
+            menuFile.add(new JMenuItem(new Save(textPane,fileChooser)));
+            menuView.add(new JMenuItem(new Search(textPane,SearchText)));
+            menuFile.add(new JMenuItem(new Exit(Editor.this)));
+            menuEdior.add(new JMenuItem(new Copy(textPane,Editor.this)));
+            menuEdior.add(new JMenuItem(new Cut(textPane,Editor.this)));
+            menuEdior.add(new JMenuItem(new Paste(textPane,Editor.this)));
             menuBar.add(menuFile);
             menuBar.add(menuEdior);
             menuBar.add(menuView);
             menuBar.add(menuAbout);
-            return menuBar;
+            return  menuBar;
         }
 
     public static void main(String[] args) {
