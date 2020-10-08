a497c53ac9c49a849dbf0c08edc850af2894a512
Administrator <850696481@qq.com>
2020-10-08 20:07:22 +0800

    test change

--- src/main/java/Editor.java
+++ src/main/java/Editor.java
@@ -18,35 +18,35 @@ public class Editor extends JFrame {
         container.add(textPane,BorderLayout.CENTER);
         setSize(700,700);
         setVisible(true);
-        }
+    }
 
-        private JMenuBar createJMenuBar(){
-            JMenuBar menuBar=new JMenuBar();
-            JMenu menuFile=new JMenu("File");
-            JMenu menuView=new JMenu("View");
-            JMenu menuEdior=new JMenu("Editor");
-            JMenu menuAbout = new JMenu("About");
-            menuFile.add(new JMenuItem(new New(textPane,fileChooser,Editor.this)));
-            menuFile.add(new JMenuItem(new Open(textPane,fileChooser,Editor.this)));
-            menuFile.add(new JMenuItem(new Save(textPane,fileChooser)));
-            menuFile.add(new JMenuItem(new Print(fileChooser,Editor.this)));
-            menuFile.add(new JMenuItem(new ExportPDF(fileChooser)));
-            menuFile.add(new JMenuItem(new Exit(Editor.this)));
-            menuView.add(new JMenuItem(new Search(textPane,SearchText)));
-            menuEdior.add(new JMenuItem(new Copy(textPane,Editor.this)));
-            menuEdior.add(new JMenuItem(new Cut(textPane,Editor.this)));
-            menuEdior.add(new JMenuItem(new Paste(textPane,Editor.this)));
-            menuBar.add(menuFile);
-            menuBar.add(menuEdior);
-            menuBar.add(menuView);
-            menuAbout.add(new JMenuItem(new Author(Editor.this)));
-            menuBar.add(menuAbout);
-            return  menuBar;
-        }
+    private JMenuBar createJMenuBar(){
+        JMenuBar menuBar=new JMenuBar();
+        JMenu menuFile=new JMenu("File");
+        JMenu menuView=new JMenu("View");
+        JMenu menuEdior=new JMenu("Editor");
+        JMenu menuAbout = new JMenu("About");
+        menuFile.add(new JMenuItem(new New(textPane,fileChooser,Editor.this)));
+        menuFile.add(new JMenuItem(new Open(textPane,fileChooser,Editor.this)));
+        menuFile.add(new JMenuItem(new Save(textPane,fileChooser)));
+        menuFile.add(new JMenuItem(new Print(fileChooser,Editor.this)));
+        menuFile.add(new JMenuItem(new ExportPDF(fileChooser)));
+        menuFile.add(new JMenuItem(new Exit(Editor.this)));
+        menuView.add(new JMenuItem(new Search(textPane,SearchText)));
+        menuEdior.add(new JMenuItem(new Copy(textPane,Editor.this)));
+        menuEdior.add(new JMenuItem(new Cut(textPane,Editor.this)));
+        menuEdior.add(new JMenuItem(new Paste(textPane,Editor.this)));
+        menuBar.add(menuFile);
+        menuBar.add(menuEdior);
+        menuBar.add(menuView);
+        menuAbout.add(new JMenuItem(new Author(Editor.this)));
+        menuBar.add(menuAbout);
+        return  menuBar;
+    }
 
     public static void main(String[] args) {
         new Editor();
     }
 
-    }
+}
 
--- src/main/java/ExportPDF.java
+++ src/main/java/ExportPDF.java
@@ -3,9 +3,16 @@ import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.Document;
 import java.awt.event.ActionEvent;
 import java.io.*;
+//import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfWriter;
 
 public class ExportPDF extends AbstractAction {
     private JFileChooser filechooser;
+
     public ExportPDF(JFileChooser fileChooser) {
         this.filechooser = fileChooser;
         fileChooser.setDialogTitle("Export to PDF");
